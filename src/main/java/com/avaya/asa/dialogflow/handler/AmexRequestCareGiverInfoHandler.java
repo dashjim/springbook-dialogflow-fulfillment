904d267c18a0e99a949ea90d94a52b6f9bc994fe
Jim <dashjim@gmail.com>
2019-09-15 17:29:29 +0800

    fix dialog content.

--- src/main/java/com/avaya/asa/dialogflow/handler/AmexRequestCareGiverInfoHandler.java
+++ src/main/java/com/avaya/asa/dialogflow/handler/AmexRequestCareGiverInfoHandler.java
@@ -2,9 +2,15 @@ package com.avaya.asa.dialogflow.handler;
 
 import com.avaya.asa.dialogflow.model.ddrequest.DialogFlowRequest;
 import com.avaya.asa.dialogflow.model.ddresponse.DialogFlowResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AmexRequestCareGiverInfoHandler implements DDHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(AmexRequestCareGiverInfoHandler.class);
+
     public static final String REQUEST_CARE_GIVER_INFO = "REQUEST CARE GIVER INFO";
+
 
     public String getIntentNameToHandle() {
         return "REQUEST CARE GIVER INFO";
@@ -12,6 +18,10 @@ public class AmexRequestCareGiverInfoHandler implements DDHandler {
 
     public DialogFlowResponse handle(DialogFlowRequest request) {
         String caregiver = HandlerHelper.getEntity(request, "CAREGIVER");
-        return caregiver.equalsIgnoreCase("primary") ? HandlerHelper.createNewDDResponseForAnswer("As the primary care giver, you can take 14 weeks of paid leave, secondary care givers can obtain up to 4 weeks of paid leave. Approved parental paid leave may be taken on a consecutive or intermittent basis following the birth, adoption or placement of a child with the employee. Do you have all the information necessary to apply for paid leave as the primary care giver?") : HandlerHelper.createNewDDResponseForAnswer("I have sent you a link, which will provide you with all the details required for the submittal of a primary or secondary paid leave request.\" (Pause)...In addition, in this link you will find training videos about the paid parental leave policy. Can I help you with something else?");
+        log.info(String.format("carevier entity is: %s", caregiver));
+        return HandlerHelper.createNewDDResponseForAnswer("As the primary care giver, you can take 14 weeks of paid leave, " +
+                "secondary care givers can obtain up to 4 weeks of paid leave. Approved parental paid leave may be taken " +
+                "on a consecutive or intermittent basis following the birth, adoption or placement of a child with the employee. " +
+                "Do you have all the information necessary to apply for paid leave as the primary care giver?");
     }
 }
