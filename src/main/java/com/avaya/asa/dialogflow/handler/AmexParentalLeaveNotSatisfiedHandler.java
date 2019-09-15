ca799f1d083f12853e3acd6de72b66dd350557a1
Jim <dashjim@gmail.com>
2019-09-15 17:32:40 +0800

    fix dialog content.

--- src/main/java/com/avaya/asa/dialogflow/handler/AmexParentalLeaveNotSatisfiedHandler.java
+++ src/main/java/com/avaya/asa/dialogflow/handler/AmexParentalLeaveNotSatisfiedHandler.java
@@ -3,8 +3,7 @@ package com.avaya.asa.dialogflow.handler;
 import com.avaya.asa.dialogflow.model.ddrequest.DialogFlowRequest;
 import com.avaya.asa.dialogflow.model.ddresponse.DialogFlowResponse;
 
-public class AmexParentalLeaveNotSatisfiedHandler implements DDHandler
-{
+public class AmexParentalLeaveNotSatisfiedHandler implements DDHandler {
 
     @Override
     public String getIntentNameToHandle() {
@@ -15,6 +14,9 @@ public class AmexParentalLeaveNotSatisfiedHandler implements DDHandler
     public DialogFlowResponse handle(DialogFlowRequest request) {
         // TODO need to know how to get the user calling number from fulfillment service so we can send SMS.
         // TODO send SMS out to customer with the link - https://about.americanexpress.com/press-release/american-express-increases-parental-leave-and- family-benefits-us-employees-beginning
-        return HandlerHelper.createNewDDResponseForAnswer("Can I help you with something else?");
+        return HandlerHelper.createNewDDResponseForAnswer(
+                "I have sent you a link, which will provide you with all the details required for the submittal of a primary or secondary paid leave request." +
+                        "In addition, in this link you will find training videos about the paid parental leave policy." +
+                        "Can I help you with something else?");
     }
 }
